a85a1805758c5b709cdc00b6ab75909ff10dce93
codecaiine <akabrouyannickn@gmail.com>
2021-11-20 20:11:40 +0000

    Final code

--- decoder.rb
+++ decoder.rb
@@ -1,29 +1,36 @@
 print 'Enter your morse code: '
 morse_str = gets.chomp
-dictionary = {
-    'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
-    'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
-    'M' => '--', 'N' => '-.', 'O' => '---', 'P' => '.--.', 'Q' => '--.-', 'R' => '.-.', 'S' => '...',
-    'T' => '-', 'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-', 'Y' => '-.--', 'Z' => '--..',
-    '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
-    '8' => '---..', '9' => '----.', '0' => '-----'
-}
-def decode_char(character)
-    return $dictionary.key(character) || ' '
-end 
 
-def decode_word(morse_text)
-    text = ''
-    morse_text.split.each do |char| text += decode_char(char)
-    end
-    text
+def decode_char(character)
+  dictionary = {
+    'A' => '.-', 'B' => '-...', 'C' => '-.-.',
+    'D' => '-..', 'E' => '.', 'F' => '..-.',
+    'G' => '--.', 'H' => '....', 'I' => '..',
+    'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.', 'O' => '---',
+    'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',
+    'S' => '...', 'T' => '-', 'U' => '..-',
+    'V' => '...-', 'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
+  }
+
+  dictionary.key(character) || ' '
 end
 
-def result(morse_result)
-    result = []
-    morse_result.split(' ').each do |text| result.push(decode_word)
-    end
-    result.join(' ')
-end    
+def decode_word(morse_word)
+  word = ''
+  morse_word.split.each do |char|
+    word += decode_char(char)
+  end
+  word
+end
 
-puts 'Your message is: #{decode_char(morse_str)}' 
+def decode_result(morse_result)
+  result = []
+  morse_result.split('   ').each do |word|
+    result.push(decode_word(word))
+  end
+  result.join(' ')
+end
+
+puts "Your message is: #{decode_result(morse_str)}"
