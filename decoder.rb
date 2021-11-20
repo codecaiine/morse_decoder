22e67ea3152d606416740f94cd4e3fdf5c6fb8f4
codecaiine <akabrouyannickn@gmail.com>
2021-11-20 20:10:22 +0000

    Create a method to decode an entire word in Morse code

--- decoder.rb
+++ decoder.rb
@@ -1,7 +1,5 @@
 print 'Enter your morse code: '
-
 morse_str = gets.chomp
-
 dictionary = {
     'A' => '.-', 'B' => '-...', 'C' => '-.-.', 'D' => '-..', 'E' => '.', 'F' => '..-.',
     'G' => '--.', 'H' => '....', 'I' => '..', 'J' => '.---', 'K' => '-.-', 'L' => '.-..',
@@ -10,9 +8,22 @@ dictionary = {
     '1' => '.----', '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
     '8' => '---..', '9' => '----.', '0' => '-----'
 }
-
 def decode_char(character)
     return $dictionary.key(character) || ' '
 end 
 
-puts 'Your message is: #{decode_char(morse_str)}'
+def decode_word(morse_text)
+    text = ''
+    morse_text.split.each do |char| text += decode_char(char)
+    end
+    text
+end
+
+def result(morse_result)
+    result = []
+    morse_result.split(' ').each do |text| result.push(decode_word)
+    end
+    result.join(' ')
+end    
+
+puts 'Your message is: #{decode_char(morse_str)}' 
